a20af710fdd65598de79994d76475a044ec6407c
GaelBriceTig <gael.tigongue@outlook.com>
2025-12-02 15:00:18 +0100

    Declaration des attribut des function permettant de gerer notre Agenda

--- src/agenda/Agenda.java
+++ src/agenda/Agenda.java
@@ -1,8 +1,35 @@
 package agenda;
 
+import taches.Tache;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Agenda {
 
+    /*
+    * on crée une liste dans la quelle on vas stocker nos differents taches
+    * */
+    List<Tache> agendas = new ArrayList<Tache>();
 
+    /*
+    * function permettant d'ajouter une tache à notre agenda*/
+    public void ajouterTache(Tache tache) {
+        this.agendas.add(tache);
+    }
 
+    /*
+    * function permettant de valider si une tache est terminé ou en cour*/
+    public Boolean marquerCommeTerminee(Tache tache) {
+        return null;
+    }
 
+    /* Tache permettant d'afficher la liste des taches presente dans ton agenda*/
+    public void afficherTaches(){
+
+        for (Tache tache : agendas) {
+            System.out.println(agendas);
+        }
+
+    }
 }
